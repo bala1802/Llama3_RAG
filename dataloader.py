c1459f66febffbdd47ba773653ad8ac3dcb97e13
bala1802 <bala1802@live.com>
2024-04-27 12:52:58 +0530

    added dataloader

---
+++ dataloader.py
@@ -0,0 +1,17 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+
+
+def load_and_retrieve_docs(url):
+    loader = WebBaseLoader(
+        web_paths=(url,),
+        bs_kwargs=dict() 
+    )
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+    return vectorstore.as_retriever()
